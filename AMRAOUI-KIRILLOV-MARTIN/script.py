f7192d00363f266b86b2eeef1f38aa6a7b39a63b
userBrian <33984499+userBrian@users.noreply.github.com>
2019-02-23 10:21:30 +0100

    conversion en tags universels

---
+++ AMRAOUI-KIRILLOV-MARTIN/script.py
@@ -0,0 +1,26 @@
+
+tags = open("test", "r")
+tagline = tags.readlines()
+dico = { }
+#print(file.read())
+for i in range(len(tagline)):
+	tagline[i] = tagline[i].split(' ')
+	dico[tagline[i][0]] = tagline[i][1].rstrip()
+
+print(dico["VBZ"])
+
+file = open("wsj_0010_sentence.pos.lima","r")
+
+output = open("output.txt", "w")
+
+line = file.readlines()
+
+line[0] = line[0].split('_')
+output.write(line[0][0])
+for i in range(1, len(line[0])):
+	print(line[0][i].split(' ')[0])
+	line[0][i] = str.replace(line[0][i], line[0][i].split(' ')[0], dico[line[0][i].split(' ')[0]])
+	output.write('_' + line[0][i])
+	#splitted = line[0][i].split('_')
+	#print(splitted[1])
+	#str.replace(splitted[1], splitted[1], dico[splitted[1]])
